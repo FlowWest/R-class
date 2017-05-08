180d36553fba802f16f7523acac4b6c3e57286a6
ergz <e.emanuel.rodriguez@gmail.com>
2017-05-08 11:45:26 -0700

    clean excel class

--- R/c3_import_data.R
+++ R/c3_import_data.R
@@ -50,3 +50,91 @@ rando <- readr::read_csv('data/rando.csv',
 
 readxl::excel_sheets()
 readxl::read_excel()
+
+# Grand Tab Subset Cleanup -----------------------------------------------------
+
+# 1 Read in raw data -----------------------------------------------------------
+
+# 2. Inspect the data, what needs work? Formulate a plan to move forward ------- 
+# use functions like View(), glimpse() and head() 
+
+# 3. Rename the year column to facilitate work on it, use colnames() function 
+
+
+# 4. The Plan: -----------------------------------------------------------------
+
+#a. Collect headers into two vectors
+
+
+#b. Combine the two headers, with a trick, and assign as new header (at the same time 
+#   removing old headers)
+
+
+#c. Gather headers into a column 
+
+
+#d. seperate column to appropriate columns 
+
+
+#e. resolve col type issues issues
+
+
+
+
+
+
+
+
+
+
+
+#### Answers -------------------------------------------------------------------
+
+# 1 ------------------------------------
+grand_tab <- read_excel("raw-data/Grand_Tab.xlsx")
+
+# 2 -----------------------------------
+View(grand_tab) # view the data in spreadsheet form 
+glimpse(grand_tab) # view details of each column 
+summary(grand_tab) # get even more detailed view for each col 
+
+# 3 ----------------------------------
+colnames(grand_tab)[1] <- "year"
+
+# 4 ----------------------------------
+# a. 
+river_reach <- colnames(grand_tab)
+species <- grand_tab[1, ] %>% as.character()
+
+# b.
+rr_species <- paste(river_reach, species, sep="-")
+rr_species <- str_replace_all(rr_species, "__1", "")
+colnames(grand_tab) <- rr_species
+grand_tab <- grand_tab[-1, ]
+
+# c. 
+grand_tab <- grand_tab %>% 
+  gather("reach_and_species", value = "total_count", 
+         `Sac_Mainstem-Fall_Run`:`Merced_River-Target_Fall_Run`) 
+
+# d. 
+grand_tab <- grand_tab %>% 
+  separate(reach_and_species, into = c("reach", "species"), sep="-")
+
+
+#e. 
+colnames(grand_tab)[1] <- "year"
+
+grand_tab$year <- as.character(grand_tab$year)
+grand_tab$total_count <- as.numeric(grand_tab$total_count)
+
+
+
+
+
+
+
+
+
+
+
